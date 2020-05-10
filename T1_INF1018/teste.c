640e6cc73214d1188dd628b94c13854e5b0bed0f
Julia Rocha <juliaafr@outlook.com>
2020-05-10 20:33:52 -0300

    8 to 32 working

--- T1_INF1018/teste.c
+++ T1_INF1018/teste.c
@@ -10,15 +10,17 @@
 #include <errno.h>
 #include "conv_utf.h"
 
-#define ENTRADA_FILENAME "ENTRADA.txt"
+#define ENTRADA8_FILENAME "/Users/juliarocha/Desktop/PUC/INF1018/SoftwareBasico/T1_INF1018/arq_entrada8.txt"
 
-#define SAIDA_FILENAME "RESULTADO.txt"
+#define ENTRADA32_FILENAME "/Users/juliarocha/Desktop/PUC/INF1018/SoftwareBasico/T1_INF1018/arq_entrada32.txt"
+
+#define SAIDA_FILENAME "/Users/juliarocha/Desktop/PUC/INF1018/SoftwareBasico/T1_INF1018/arq_saida.txt"
 
 int main (void) {
     FILE *entrada,*saida;
     errno = 0;
     
-    entrada = fopen(ENTRADA_FILENAME,"rb");
+    entrada = fopen(ENTRADA32_FILENAME,"rb");
     saida = fopen(SAIDA_FILENAME, "wb");
     
     if (entrada == NULL) {
