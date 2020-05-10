ff58288a53973dd6a3beb9fb2c561d605081428a
Julia Rocha <juliaafr@outlook.com>
2020-05-10 20:35:55 -0300

    8 to 32 working

--- T1_INF1018/conv_utf.c
+++ T1_INF1018/conv_utf.c
@@ -6,314 +6,162 @@
 //  Copyright © 2020 Julia Rocha. All rights reserved.
 //
 
-#include "conv_utf.h"
 #include <stdio.h>
 #include <string.h>
+#include "conv_utf.h"
 
-// macros for errors; though EOF is system dependent,
-// it is defined as negative, so all errors are < 0.
-#define ERR_READ  EOF-1 // probably -2
-#define ERR_WRITE EOF-2 // probably -3
-#define ERR_PARSE EOF-3 // error when read character is not valid UTF
-#define ERR_END   EOF-4 // error when file's endian is not valid
-#define SUCCESS   1     //everything went smooth
-
-//Imprime o conteudo de certo endereço de memoria (apenas para debug)
-static void dump_char (void *p, int n, char *sep){
-    unsigned char *p1 = p;
-    sep = sep ? sep : " ";
-    while (n--) {
-        printf("%02x%s", *p1, sep);
-        p1++;
-    }
-    puts("");
-}
-
-//Caso exista algum erro no arquivo, imprime e retorna erro
-//No caso de EOF, apenas retoran EOF sem imprimir
-static int get_err(FILE *f){
-    int err = SUCCESS;
-    if (ferror(f)){
-        perror("Erro ao ler arquivo:\n");
-        err = ERR_READ;
-    }
-    else if (feof(f))
-        err = EOF;
-    return err;
-}
-
-//Descobre se a ordenação do arquivo utf-32 é big endian ou little endian
-static char endian(FILE * f){
-    char ret;
-    char read[4];
-    char big[4] = {'\0'  , '\0'  , '\376', '\377'};
-    char lit[4] = {'\377', '\376', '\0'  , '\0'};
-    
-    fread(read, 1, 4, f);
-
-    //if everything's ok
-    if ((ret = get_err(f)) == SUCCESS){
-        int i;
-        char * expected;
-
-        // if read begins like big, expected should be big
-        if      (read[0] == big[0]){
-            expected = big;
-            ret = 'B';
-        }
-        // if read begins like little, expected should be little
-        else if (read[0] == lit[0]){
-            expected = lit;
-            ret = 'L';
-        }
-        // if read is different, file is bizarre and returns error
-        else {
-            return ERR_END;
-        }
-        for (i=1; i<4; i++)
-            ret = read[i] == expected[i] ? ret : ERR_END;
-    }
-    return ret;
-}
-
-//Coloca no parametro 'c' o valor do próximo caracter UTF-32
-static int next_char_32(FILE *in_file, unsigned char c[]){
-    int err;
-    fread(c, 1, 4, in_file);
-    if ((err = get_err(in_file)) != SUCCESS)
-        return err;
-    return SUCCESS;
-}
-
-//Retorna em que intervalo de representação de UTF-32 o caracter lido está
-static int interval_32_lit (unsigned char r_char[]){
-    unsigned int c = *(unsigned int *)r_char;
-    int ret;
-    if      (c < 0 || c > 0x10ffffu) { //out of bounds
-        ret = ERR_PARSE;
-    }
-    else if (c <= 0x007fu)
-        ret = 1;
-    else if (c <= 0x07ffu)
-        ret = 2;
-    else if (c <= 0xffff)
-        ret = 3;
-    else if (c <= 0x10ffffu)
-        ret = 4;
-    return ret;
-}
-
-//"Entende" (atraves da func anterior) e converte o caracter lido com ordenação little endian para UTF-8
-static int parse_32_lit (unsigned char r_char[], unsigned char conv[]){
-    int inter = interval_32_lit(r_char);
-
-    switch(inter) {
-        case 1:
-            conv[0] = 0 | r_char[0]; //begins with 0 and the rest is the utf code
-            conv[1] = 0;     //EOS
-            break;
-        case 2:
-            conv[0] = (r_char[0] >> 6) | (r_char[1] << 2) | 0xc0u;
-            conv[1] = (r_char[0] & 0x3fu) | 0x80u;
-            conv[2] = 0;     //EOS
-            break;
-        case 3:
-            conv[0] = ((r_char[1] & 0xf0u) >> 4) | 0xe0u;
-            conv[1] = (r_char[0] >> 6) | ((r_char[1] & 0x0fu) << 2) | 0x80u;
-            conv[2] = (r_char[0] & 0x3fu) | 0x80u;
-            conv[3] = 0;     //EOS
-            break;
-        case 4:
-            conv[0] = ((r_char[2] & 0x1cu) >> 2) | 0xf0u;
-            conv[1] = ((r_char[1] & 0xf0u) >> 4) | (r_char[2] & 0x03u) | 0x80u ;
-            conv[2] = (r_char[0] >> 6) | ((r_char[1] & 0x0fu) << 2) | 0x80u;
-            conv[3] = (r_char[0] & 0x3fu) | 0x80u;
-            conv[4] = 0;     //EOS
-            break;
-        default:
-            break;
-    }
-
-    return inter;
-}
-
-//Altera a endianness de c
-static void big2lit (unsigned char big[], unsigned char lit[]){
+void conv_little_endian(unsigned int * n) {
+    unsigned int aux, saida, mask=0xFF;
     int i;
-    for (i=0; i<4; i++)
-        lit[3-i] = big[i];
-}
 
-//Converte o caracter UTF-32 the big para little endian
-//e entao usa a parse_32_lit
-static int parse_32_big(unsigned char c[], unsigned char conv[]){
-    unsigned char lit[4];
-    big2lit(c, lit);
-    return parse_32_lit(lit, conv);
-}
+    saida=0x0;
 
-int utf32_8(FILE *arq_entrada, FILE *arq_saida){
-    int err;
-    unsigned char r_char[4];
-    unsigned char conv[5]; // maximum of 4 bytes in a utf-8 + EOS
-    int (*parse)(unsigned char[], unsigned char []);
-
-    // sets callback accordingly while checking for possible errors
-    switch(endian(in_file)){
-        case 'L':
-            parse = parse_32_lit; break;
-        case 'B':
-            parse = parse_32_big; break;
-        default:
-            printf("Erro! Caracter UTF-32 invalido na posicao %ld.\n", ftell(in_file));
-            return -1;
+    for( i = 0; i < 4; i++) {
+        aux= mask & *n;
+        aux<<=(24-8*i);
+        saida = saida | aux;
+        (*n)>>=8;
     }
 
-    err = next_char_32(in_file, r_char);
-    while (err != EOF) {
-        if (err == ERR_READ) //could not read
-            return -1;
+    *n=saida;
+    return;
+}
 
-        if (parse(r_char, conv) == ERR_PARSE){ // bad character
-            fprintf(stderr, "Erro! Caracter UTF-32 invalido na posicao %ld.\n", ftell(in_file));
+char GET_BOM (unsigned int byte_bom ) {
+    if ( byte_bom == 0Xfffe0000 )
+        return 'b';
+    if ( byte_bom == 0xFEFF )
+        return 'l';
+    return 'e';
+}
+
+int utf8_32(FILE *arq_entrada, FILE *arq_saida) {
+    unsigned int saida, aux;
+    int num_byte = 1;
+    unsigned char byte_lido;
+    int qtd_bytes = 1;
+    int i;
+    
+    while (fread(&byte_lido, 1, 1, arq_entrada) == 1) {
+        while(0x8000 & byte_lido) {
+            qtd_bytes++;
+            byte_lido <<= 1;
+        }
+
+        if (qtd_bytes > 4) {
+            printf("Erro no byte %d",num_byte);
             return -1;
         }
 
-        fprintf(out_file, "%s", conv);
-        if (ferror(out_file)){ //could not write
-            perror("Erro ao escrever arquivo:\n");
-            return -1;
-        }
-        err = next_char_32(in_file, r_char);
-    }
+        if (qtd_bytes == 1)
+            saida=(0x0 | byte_lido);
+        else {
+            aux = byte_lido;
 
+            aux <<= (4*(qtd_bytes-2));
+
+            saida = (0x0 | aux);
+
+            for(i = 0; i < qtd_bytes; i++) {
+                
+                fread(&byte_lido, 1, 1, arq_entrada);  // le proximo byte
+
+
+                if(!(0x8000 & byte_lido) || (0x4000 & byte_lido) ) {
+                    printf("Erro no byte %d",num_byte);
+                    return -1;
+                }
+
+                aux = byte_lido;
+                aux = (aux & 0x7FFF);  // zera primeiro byte
+                aux <<= (6*(qtd_bytes-2-i)); // shifta até a posição correta
+                saida = (saida | aux);
+            }
+        }
+        fwrite(&saida, 4, 1, arq_saida); //imprime
+        num_byte++;
+    }
     return 0;
 }
 
-/****************** Begin utf8_32 *******************/
+int utf32_8( FILE *arq_entrada , FILE *arq_saida ){
 
-//Escreve na saida a sequencia caracteristica de big ou little endian
-static int write_endian(FILE *out, int is_big){
-    char big[4] = {'\0'  , '\0'  , '\376', '\377'};
-    char lit[4] = {'\377', '\376', '\0'  , '\0'};
-    char *seq = is_big ? big : lit;
-    fwrite(seq, 1, 4, out);
-    if (ferror(out)){ //could not write
-        perror("Erro ao escrever arquivo.\n");
-        return ERR_WRITE;
-    }
-    return SUCCESS;
-}
+    unsigned int buffer_entrada , aux , bytes=0;
 
-//Converte de UTF-8 para UTF-32 com ordenação big endian
-static int parse_8_2big(unsigned char r_char[], int r_nbytes, unsigned char conv[]){
-    int i;
+    unsigned char bom, buffer_saida [ 4 ] = { 0 , 0x80 , 0x80 , 0x80 };
 
-    // if nbytes > 1, checks if every byte is 10xx xxxx
-    for (i=1; i<r_nbytes; i++)
-        if ((r_char[i] & 0xc0u) != 0x80u)
-            return ERR_PARSE;
+    unsigned long elements,escritos;
+    int n , i ;
 
-    switch(r_nbytes){
-        case 1:
-            memset(conv, 0, 3); //first 3 bytes are 0
-            conv[3] = r_char[0];
-            break;
-        case 2:
-            memset(conv, 0, 2); //first two bytes are 0
-            conv[2] = (r_char[0] >> 2) & 0x07u;
-            conv[3] = ((r_char[0] & 0x03u) << 6) | (r_char[1] & 0x3fu);
-            break;
-        case 3:
-            memset(conv, 0, 2); //first two bytes are 0
-            conv[2] = (r_char[0] << 4) | ((r_char[1] >> 2) & 0x0fu);
-            conv[3] = (r_char[1] << 6) | (r_char[2] & 0x3fu);
-            break;
-        case 4:
-            conv[0] = 0; //first byte is 0
-            conv[1] = ((r_char[0] & 0x07u) << 2) | ((r_char[1] & 0x30u) >> 4);
-            conv[2] = (r_char[1] << 4) | ((r_char[2] & 0x3cu) >> 2);
-            conv[3] = (r_char[2] << 6) | (r_char[3] & 0x3du);
-
-            printf("Got:  "); dump_char(r_char, r_nbytes, NULL);
-            printf("Conv: "); dump_char(conv, 4, NULL);
-            getchar();
-
-            break;
-    }
-
-    return SUCCESS;
-}
-
-//Converte o caracter UTF-8 para UTF-32 big endian
-//e entao modifica sua ordenação para little endian
-static int parse_8_2lit(unsigned char r_char[], int r_nbytes, unsigned char conv[]){
-    int err;
-    unsigned char conv2[4];
-    if ((err = parse_8_2big(r_char, r_nbytes, conv2)) != SUCCESS)
-        return err;
-
-    big2lit(conv2, conv);
-    return SUCCESS;
-}
-
-//Le o proximo caracter utf-8 e retorna o numero de bytes que o caracter em UTF-8 ocupa
-static int next_char_8(FILE * f, unsigned char c[]){
-    unsigned char r_char, mask;
-    int err, nbytes;
-
-    r_char = c[0] = fgetc(f);
-
-    //checks for errors / EOF
-    if ((err = get_err(f)) != SUCCESS)
-        return err;
-
-    //counts number of bytes on utf8 character
-    for (nbytes=0, mask=0x80u; r_char & mask; mask>>=1, nbytes++);
-    //invalid char or something went wrong
-    if (nbytes == 1 || nbytes > 4){
-        fprintf(stderr, "Erro! Caracter UTF-8 invalido na posicao %ld:\n", ftell(f));
-        return ERR_PARSE;
-    }
-
-    // if only one byte, for won't loop, so nbytes will be 0
-    nbytes = nbytes ? nbytes : nbytes+1;
-    //reads nbytes-1, because of fgetc on start of this function
-    fread(c+1, 1, nbytes-1, f);
-    if ((err = get_err(f)) != SUCCESS)
-        return err;
-
-    return nbytes;
-}
-
-int utf8_32(FILE *arq_entrada, FILE *arq_saida){
-    unsigned char r_char[4], conv[4];
-    int r_nbytes;
-    int order = 1;
-    int (*parse)(unsigned char[], int, unsigned char []) = order ? parse_8_2big : parse_8_2lit;
-
-    if (write_endian(out_file, order) != SUCCESS)
+    if (fread(&buffer_entrada, 4, 1, arq_entrada) == 1) {
+        fprintf(stderr,"\nErro de entrada: Arquivo termina inesperadamente no byte %u\n",bytes);
         return -1;
-
-    if ((r_nbytes = next_char_8(in_file, r_char)) < 0)
+    }
+    
+    bom = GET_BOM(buffer_entrada);
+    
+    if (bom == 'b') {
+        conv_little_endian(&buffer_entrada);
+    }
+    
+    if (bom == 'e') {
+        fprintf(stderr,"\nErro de entrada: caracter BOM em formato errado:\n");
         return -1;
+    }
+    
+    do {
+        
+        elements = fread(&buffer_entrada , 4 , 1 , arq_entrada);
 
-    while(!(feof(in_file))){
-        if (parse(r_char, r_nbytes, conv) == ERR_PARSE){
-            fprintf(stderr, "Erro! Caracter UTF-8 invalido na posicao %ld:\n", ftell(in_file));
+        bytes += 4;
+
+        if (elements == 0 )
+            return 0;
+        if (bom == 'b') {
+            conv_little_endian ( &buffer_entrada );
+        }
+        if (buffer_entrada > 0x10FFFF) {
+            fprintf(stderr,"\nErro de entrada: Algarismo não existe na representação UNICODE. Byte: %u\n",bytes);
             return -1;
         }
-
-        fwrite(conv, 1, 4, out_file);
-        if (ferror(out_file)){ //could not write
-            perror("Erro ao escrever arquivo.\n");
+        if (elements != 1) {
+            fprintf(stderr,"\nErro de entrada: Arquivo corrompido, terminou em um estado instável no byte %u\n",bytes);
             return -1;
         }
+        
+        n = 1;
+        aux = 0xFFFFFF80;
 
-        r_nbytes = next_char_8(in_file, r_char);
-        if (r_nbytes == ERR_PARSE && r_nbytes == ERR_READ)
-            return -1;
+        if (aux & buffer_entrada) {
+            n++;
+            aux <<= 4 ;
+            while (aux & buffer_entrada) {
+                n++ ;
+                aux <<= 5 ;
+            }
+        }
+        aux = 0x80; // aux agora serve pra colocar os 1's no byte inicial
+        buffer_saida[0] = 0;
+        
+        for (i = 1; i < 4; i++)
+            buffer_saida[i]=0x80;
+        
+        for (i = 0 ; i < n; i++) {
+            if (n > 1) {
+                buffer_saida [ 0 ] = buffer_saida [ 0 ] | ( aux >> i );
+                buffer_saida[i] = (buffer_saida[i] | ( ( buffer_entrada >> ( 6 * (n-1-i) ) ) & 0x3F ) );
+            } else {
+                buffer_saida[0] = buffer_entrada;
+            }
+            buffer_saida[i] = (buffer_saida[i] | ( ( buffer_entrada >> ( 6 * (n-1-i) ) ) & 0x3F ) );
+        }
+        
+        escritos = fwrite(buffer_saida , 1 , n , arq_saida);
+        
+        if (escritos != n) {
+            fprintf(stderr,"Erro de saida: Nao foi possivel escrever no arquivo");
+        }
     }
-
+    while (elements == 1);
     return 0;
 }
